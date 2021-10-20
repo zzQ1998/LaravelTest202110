935269022684b5627d70aaf435aedb4925956445
“ZZQ” <il>
2021-10-20 22:09:57 +0800

    项目初始化20211020

---
+++ app/Http/Controllers/aaaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class aaaController extends Controller
+{
+    //
+        public function __construct()
+        {
+
+        }
+}
